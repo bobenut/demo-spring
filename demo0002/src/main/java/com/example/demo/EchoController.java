67a24eb92bab0af376e8c5f4798195904daef20d
bobenut <33755383@qq.com>
2017-08-17 16:16:35 +0800

    done

--- demo0002/src/main/java/com/example/demo/EchoController.java
+++ demo0002/src/main/java/com/example/demo/EchoController.java
@@ -8,31 +8,37 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @RestController
 @RequestMapping("/echo")
 public class EchoController {
-	private static final String echoTemplate = "received %s!";
-	private static final String messageTemplate = "%s speak to %s \'%s\'";
+	private static final String echoTemplate1 = "received %s!";
+	private static final String echoTemplate2 = "%s speak to %s \'%s\'";
     private final AtomicLong counter = new AtomicLong();
     
     @RequestMapping(value="/getter/pattern1", method=RequestMethod.GET)
     public Echo getterPattern1(String content) {
-        return new Echo(counter.incrementAndGet(), String.format(echoTemplate, content));
+        return new Echo(counter.incrementAndGet(), String.format(echoTemplate1, content));
     }
     
     @RequestMapping(value="/getter/pattern2", method=RequestMethod.GET)
     public Echo getterPattern2(@RequestParam(value="content", required=false) String alias) {
-        return new Echo(counter.incrementAndGet(), String.format(echoTemplate, alias));
+        return new Echo(counter.incrementAndGet(), String.format(echoTemplate1, alias));
     }
     
     @RequestMapping(value="/getter/pattern3/{content}", method=RequestMethod.GET)
     public Echo getterPattern3(@PathVariable String content) {
-        return new Echo(counter.incrementAndGet(), String.format(echoTemplate, content));
+        return new Echo(counter.incrementAndGet(), String.format(echoTemplate1, content));
     }
     
     @RequestMapping(value="/setter/message1", method=RequestMethod.POST)
     public Echo setterMessage1(@RequestBody Message message) {
-        return new Echo(counter.incrementAndGet(), String.format(messageTemplate, message.getFrom(), message.getTo(), message.getContent()));
+        return new Echo(counter.incrementAndGet(), String.format(echoTemplate2, message.getFrom(), message.getTo(), message.getContent()));
+    }
+    
+    @RequestMapping(value="/setter/message2", method=RequestMethod.POST)
+    public Echo setterMessage2(@ModelAttribute Message message) {
+        return new Echo(counter.incrementAndGet(), String.format(echoTemplate2, message.getFrom(), message.getTo(), message.getContent()));
     }
 }
--- demo0002/src/main/java/com/example/demo/Message.java
+++ demo0002/src/main/java/com/example/demo/Message.java
@@ -18,4 +18,16 @@ public class Message {
 	public String getContent() {
 		return this.content;
 	}
+	
+	public void setFrom(String value) {
+		this.from = value;
+	}
+	
+	public void setTo(String value) {
+		this.to = value;
+	}
+	
+	public void setContent(String value) {
+		this.content = value;
+	}
 }
