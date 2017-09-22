fe62124f5b23e680587c9f7f7b7f2cd4a4be57c9
bobenut <33755383@qq.com>
2017-09-22 16:33:09 +0800

    done

--- demo0003/src/main/java/com/kxh/example/demo/dao/ContactsRepository.java
+++ demo0003/src/main/java/com/kxh/example/demo/dao/ContactsRepository.java
@@ -2,9 +2,6 @@ package com.kxh.example.demo.dao;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.jpa.repository.query.Procedure;
-import org.springframework.data.repository.query.Param;
 
 import com.kxh.example.demo.domain.Contact;
 
